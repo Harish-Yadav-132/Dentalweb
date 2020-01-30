fd70989f13a1b97c4d80f778884cebb29d6d2ed7
Harish-Yadav-132 <harishyadav132@gmail.com>
2020-01-30 08:39:18 +0530

    url routing ,views, static & template rendering set and ready to go

--- Dental/Dental/main/urls.py
+++ Dental/Dental/main/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('blogdetails/', views.blogdetails, name='blogdetails'),
+    path('contactus/', views.contactus, name='contact'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('service/', views.service, name='service'),
+
 ]
--- Dental/Dental/main/views.py
+++ Dental/Dental/main/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'main/home.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def blog(request):
+    return render(request, 'main/blog.html')
+
+
+def blogdetails(request):
+    return render(request, 'main/blog-details.html')
+
+
+def contactus(request):
+    return render(request, 'main/contact.html')
+
+
+def pricing(request):
+    return render(request, 'main/pricing.html')
+
+
+def service(request):
+    return render(request, 'main/service.html')
